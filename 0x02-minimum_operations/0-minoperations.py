5f6d32e52396072e2ed39d96febc0cbbd43e1d3a
Celeekeybor <celestinbor02@gmail.com>
2024-03-23 08:34:12 -0700

    initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+minimum operations
+"""
+
+
+def minOperations(n):
+    """
+    Calculate the minimum
+    number of operations required to achieve 'n' characters in a file.
+
+    Parameters:
+    n (int): The target number of characters.
+
+    Returns:
+    int: The minimum number of operations required.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2
+
+    while i * i <= n:
+        while n % i == 0:
+            operations += i
+            n //= i
+        i += 1
+
+    if n > 1:
+        operations += n
+
+    return operations
