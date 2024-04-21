24c18675fa427a4fbc7b37b345edfe2d944e10c4
Celeekeybor <celestinbor02@gmail.com>
2024-04-21 12:34:48 -0700

    add

--- 0x06-starwars_api/0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,30 @@
 #!/usr/bin/node
+/* eslint-disable */
+/* eslint-env es6 */
 
-const request = require('request');
-
+const { promisify } = require('util');
+const request = promisify(require('request'));
 const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
-
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
+async function fetchCharacterData(characterId) {
+  try {
+    const { body } = await request(characterId);
+    console.log(JSON.parse(body).name);
+  } catch (error) {
+    console.error(`Error fetching character: ${error.message}`);
   }
-};
+}
 
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
+(async () => {
+  try {
+    const { body } = await request(url);
+    const characters = JSON.parse(body).characters;
 
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
+    for (const characterId of characters) {
+      await fetchCharacterData(characterId);
     }
+  } catch (error) {
+    console.error(`Error fetching movie data: ${error.message}`);
   }
-};
-
-getCharNames();
+})();
